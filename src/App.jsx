feae075393e1c896a087a943002378e46613ed9d
SachiniPinnaduwa <sachinipinnaduwa2@gmail.com>
2025-02-24 22:44:32 +0530

    Trending movies feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useDebounce } from "react-use";
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import { updateSearchCount } from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -49,6 +50,10 @@ const App = () => {
       //console.log(data);
 
       setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error("Error fetching movies: ${error}");
       setErrorMessage("Error fetching movies. Please try again later");
